640230ea862dca2455e9c94e789bee45bac073f6
Vynokur Yurii <vynokur.yurii@gmail.com>
2024-03-07 19:47:20 +0200

    add split bill logic

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,22 +1,47 @@
 import Button from "./Button";
+import {useState} from "react";
 
 export default function FormSplitBill({selectedFriend}) {
+  const [bill, setBill] = useState(0);
+  const [paidByUser,  setPaidByUser] =  useState(0);
+  const [whoIsPaying, setWhoIsPaying] = useState('user');
+  const paidByFriend = bill ? bill - paidByUser : '';
+
+  function handleSetPaidByUser(value) {
+    const correctPaid = value <= bill ? value : paidByUser;
+    setPaidByUser(correctPaid);
+  }
+
   return <form className="form-split-bill">
     <h2>Split a bill with {selectedFriend.name}</h2>
 
     <label>Bill value</label>
-    <input type="text"/>
+    <input
+      type="text"
+      value={bill}
+      onChange={(e) => setBill(Number(e.target.value))}
+    />
 
     <label>Your expense</label>
-    <input type="text"/>
+    <input
+      type="text"
+      value={paidByUser}
+      onChange={(e) => handleSetPaidByUser(Number(e.target.value))}
+    />
 
     <label>{selectedFriend.name}'s expense</label>
-    <input type="text" disabled />
+    <input
+      type="text"
+      value={paidByFriend}
+      disabled
+    />
 
     <label>Who is paying the bill?</label>
-    <select>
-      <option value={0}>You</option>
-      <option value={1}>{selectedFriend.name}</option>
+    <select value={whoIsPaying}
+            onChange={(e) => setWhoIsPaying(e.target.value)}
+    >
+      <option value='user'>You</option>
+      <option value='friend'>{selectedFriend.name}</option>
     </select>
 
     <Button>Split bill</Button>
